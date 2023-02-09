4b5542fa3caceb52f915419a222cba4bb157fd32
Chris <cbega1227@gmail.com>
2023-02-09 11:05:22 -0700

    Set up link route /dashboard & /form-page/login to direct from login or sign up page to dashboard.

--- studdy-buddies/src/App.js
+++ studdy-buddies/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import in pages as we go
 import HomePage from "./pages/HomePage";
 import FormPage from "./pages/FormPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/form-page" element={<FormPage />}></Route>
+        <Route path="/dashboard" element={<DashboardPage />}></Route>
       </Routes>
     </Router>
   );
---
+++ studdy-buddies/src/pages/DashboardPage.jsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+    </>
+  );
+}
