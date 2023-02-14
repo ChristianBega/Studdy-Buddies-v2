860bbf91e268b973cf09b085dccb3be7185d79e9
Chris <cbega1227@gmail.com>
2023-02-13 19:01:48 -0700

    Working on subtopics pages.

--- studdy-buddies/src/components/SubTopic/CodeDemo.jsx
+++ studdy-buddies/src/components/SubTopic/CodeDemo.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Add from "./Add";
 
 export default function CodeDemo() {
   return (
     <div className="subtopicBox subtopicCodeDemo">
       <h3>Code Demo</h3>
+      {/* <button onClick={handleSubmit}>Add</button> */}
+      <Add />
       <code>Enter your code example here...</code>
     </div>
   );
