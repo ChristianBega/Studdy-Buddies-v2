5c0dabb744ac6993f7c265a6e056629eb6d7aec5
Chris <cbega1227@gmail.com>
2023-02-14 14:29:38 -0700

    Completed resources drop down menu.

--- studdy-buddies/src/components/SubTopic/Resources.jsx
+++ studdy-buddies/src/components/SubTopic/Resources.jsx
@@ -1,17 +1,52 @@
-// import { OptionUnstyled, SelectUnstyled } from "@mui/base";
-import { OptionUnstyled, SelectUnstyled } from "@mui/base";
 import React from "react";
 
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+import "../../Styles/SubtopicPage.css";
+import { InputLabel } from "@mui/material";
+
+const resourceMockData = [
+  {
+    resourceName: "Semantic HTML ",
+    resourceLink: "https://developer.mozilla.org/en-US/docs/Glossary/Semantics",
+    id: 1,
+  },
+  {
+    resourceName: "Accessibility (ARIA)",
+    resourceLink: "https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA",
+    id: 2,
+  },
+
+  {
+    resourceName: "HTML attributes",
+    resourceLink: "https://developer.mozilla.org/en-US/docs/Glossary/Attribute",
+    id: 3,
+  },
+];
+
 export default function Resources() {
+  const [resource, setResource] = React.useState();
+  const handleChange = (event) => {
+    setResource(event.target.value);
+  };
+
   return (
     <div className="subtopicBox subtopicResources">
-      {/* <label for="resources">Resources</label>
-       */}
       <h3>Resources</h3>
-      <SelectUnstyled defaultValue={10}>
-        <OptionUnstyled value={25} />
-      
-      </SelectUnstyled>
+      <FormControl variant="standard">
+        <InputLabel id="resource">{resource || "Resources"}</InputLabel>
+        <Select labelId="resources" id="demo-simple-select-standard" onChange={handleChange}>
+          {resourceMockData.map((resource) => (
+            <MenuItem value={resource.resourceName}>
+              <a href={resource.resourceLink} target="_blank" rel="noopener noreferrer">
+                {resource.resourceName}
+              </a>
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
   );
 }
