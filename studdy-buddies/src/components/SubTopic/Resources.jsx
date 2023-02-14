acd8c75b5e2c985a2ef3310ee71408c1998a1e0f
Chris <cbega1227@gmail.com>
2023-02-14 11:23:53 -0700

    Merging with main

--- studdy-buddies/src/components/SubTopic/Resources.jsx
+++ studdy-buddies/src/components/SubTopic/Resources.jsx
@@ -1,3 +1,5 @@
+// import { OptionUnstyled, SelectUnstyled } from "@mui/base";
+import { OptionUnstyled, SelectUnstyled } from "@mui/base";
 import React from "react";
 
 export default function Resources() {
@@ -6,12 +8,10 @@ export default function Resources() {
       {/* <label for="resources">Resources</label>
        */}
       <h3>Resources</h3>
-      <select name="resources" id="resources">
-        <option value="resource1">Resource1</option>
-        <option value="resource2">Resource2</option>
-        <option value="resource3">Resource3</option>
-        <option value="resource4">Resource4</option>
-      </select>
+      <SelectUnstyled defaultValue={10}>
+        <OptionUnstyled value={25} />
+      
+      </SelectUnstyled>
     </div>
   );
 }
