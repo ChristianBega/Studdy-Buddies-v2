b000a6ce73d32f289bd58df8705fa2dd25de9fa2
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-02-14 17:16:12 -0700

    styled the topics container

--- studdy-buddies/src/components/DashBoard/Card.jsx
+++ studdy-buddies/src/components/DashBoard/Card.jsx
@@ -1,15 +1,23 @@
-import NextButton from "../Buttons/NextButton"
+import NextButton from "../Buttons/NextButton";
 import BackButton from "../Buttons/BackButton";
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function Card () {
     return (
         <div className="topicContainer">
+            <div className="innerContainer">
+                <a>JavaScript</a>
+            </div>
             <div className="outerContainer">
-                <div className="innerContainer">
-                    <a>JavaScript</a>
-                </div>
                 <NextButton />
                 <BackButton />
+                <FavoriteBorderIcon className="favoriteIcon"/>
+            </div>
+            <div className="arrowContainer">
+                <ArrowBackIcon className="arrow"/>
+                <ArrowForwardIcon className="arrow"/>
             </div>
         </div>
     );
