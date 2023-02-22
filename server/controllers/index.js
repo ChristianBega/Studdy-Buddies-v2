904615870506e448949202376f28fd0b2e940209
Chris <cbega1227@gmail.com>
2023-02-22 10:27:23 -0700

    Added home-routes.js to controllers.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,12 +2,12 @@
 const router = require("express").Router();
 
 // Requiring in routes
-const apiRoutes = require("./api");
+// const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
 
 // Route prefixes
 router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
 // Exporting Routes
 module.exports = router;
